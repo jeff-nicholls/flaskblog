1b469682faf7f23a42bfd96708e0d62451e4a348
jeff-nicholls <54220470+jeff-nicholls@users.noreply.github.com>
2019-08-21 21:05:32 -0400

    Forgot to include flasblog.py in last commit

--- flaskblog.py
+++ flaskblog.py
@@ -1,14 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Jeff Nicholls',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 21, 2019'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 22, 2019'
+    }
+]
 
 @app.route('/')
 @app.route('/home')
 def home():
-        return 'Home Page'
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
-    return 'About Page'
+    return render_template('about.html', title='About')
 
 if __name__ == '__main__':
     app.run(debug=True)
