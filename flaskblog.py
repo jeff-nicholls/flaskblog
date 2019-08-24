0672b6d507a4d3c90f3cdf420edb15d39d377266
jeff-nicholls <54220470+jeff-nicholls@users.noreply.github.com>
2019-08-23 22:13:21 -0400

    Started third tutorial, just started login and register.html

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '32ab17476817326e6707176f572e9f42'
 
 posts = [
     {
@@ -24,6 +27,16 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
